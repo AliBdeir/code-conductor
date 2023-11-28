1041c80e467eadf1536a0c7a4002829ba2283435
khanabdu25 <khanabdu@umich.edu>
2023-11-28 13:56:34 -0500

    Padding on main div

--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -2,7 +2,7 @@ import './main-page.css'
 
 function MainPage() {
     return (
-        < div >
+        < div style={{ padding: '16px' }}>
             { /* // ! HTML GOES HERE */}
             <h1>Main Page</h1>
         </div >
