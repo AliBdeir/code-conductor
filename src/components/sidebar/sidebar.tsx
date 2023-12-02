1bc8aaf922f90539882dc3a3e247699160905435
khanabdu25 <khanabdu@umich.edu>
2023-12-02 11:03:44 -0500

    Made sidebar responsive & changed its content
    - Made sidebar responsive
    - Using useState hook to set isCollapsed state variable
    - Icon changes based on state
    - Basic outline of sidebar content now present
    - Mixins used to animate smoothly (taken from MIUI mini drawer variant code)
    - Various icons used to mark sidebar sections

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,40 +1,204 @@
 import BackIcon from '@mui/icons-material/ArrowBackIos';
-import MailIcon from '@mui/icons-material/Mail';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import { Divider, Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Drawer, Toolbar, IconButton, Divider, List, ListItemButton, ListItemText } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Tooltip from '@mui/material/Tooltip';
+import LabelIcon from '@mui/icons-material/Label';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import TransitEnterexitIcon from '@mui/icons-material/TransitEnterexit';
+import CallMadeIcon from '@mui/icons-material/CallMade';
 
-const drawerWidth = 240;
+
+const drawerWidth = 280;
+const thinDrawerWidth = 60;
+
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: thinDrawerWidth,
+    [theme.breakpoints.up('sm')]: {
+        width: thinDrawerWidth,
+    },
+});
 
 const Sidebar = () => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
     const theme = useTheme();
+
+    const handleToggleDrawer = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
     return (
         <Drawer
             variant="persistent"
             anchor='left'
             open={true}
             sx={{
-                width: drawerWidth,
-                flexShrink: 0,
+                ...(isCollapsed ? closedMixin(theme) : openedMixin(theme)),
                 '& .MuiDrawer-paper': {
-                    width: drawerWidth,
+                    width: isCollapsed ? thinDrawerWidth : drawerWidth,
                     boxSizing: 'border-box',
                 },
             }}
         >
-            <Toolbar className='h-16 flex justify-end' style={{ backgroundColor: theme.palette.primary.main }}>
-                <IconButton style={{ color: theme.palette.primary.contrastText }}><BackIcon /></IconButton>
+            <Toolbar
+                className='h-16'
+                style={{
+                    backgroundColor: theme.palette.primary.main,
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <IconButton
+                    style={{
+                        color: theme.palette.primary.contrastText,
+                    }}
+                    onClick={handleToggleDrawer}
+                >
+                    {isCollapsed ? <MenuIcon /> : <BackIcon />}
+                </IconButton>
             </Toolbar>
+
+
             <Divider />
             <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItemButton key={text}>
+                {/* Algorithm Name */}
+                <Tooltip title="Algorithm Name" placement="right">
+                    <ListItemButton>
                         <ListItemIcon>
-                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            <LabelIcon />
                         </ListItemIcon>
-                        <ListItemText primary={text} />
+                        {!isCollapsed && <ListItemText primary="Algorithm Name" />}
                     </ListItemButton>
-                ))}
+                </Tooltip>
+                {!isCollapsed && (
+                    <ListItemButton>
+                        <TextField
+                            id="algorithm-name"
+                            label="Algorithm Name"
+                            variant="outlined"
+                            size="small"
+                            fullWidth
+                        />
+                    </ListItemButton>
+                )}
+                <Divider />
+                {/* Input Parameters */}
+                <Tooltip title="Input Parameters" placement="right">
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <TransitEnterexitIcon />
+                        </ListItemIcon>
+                        {!isCollapsed && <ListItemText primary="Input Parameters" />}
+                    </ListItemButton>
+                </Tooltip>
+                {!isCollapsed && (
+                    <ListItemButton>
+                        <TextField
+                            id="input-name"
+                            label="Inputs"
+                            variant="outlined"
+                            size="small"
+                            fullWidth
+                            margin="dense"
+                        />
+                        <TextField
+                            id="input-data-type"
+                            label="Data Type"
+                            variant="outlined"
+                            size="small"
+                            fullWidth
+                            margin="dense"
+                        />
+                    </ListItemButton>
+                )}
+                <Divider />
+                {/* Output Parameters */}
+                <Tooltip title="Output Parameters" placement="right">
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <CallMadeIcon />
+                        </ListItemIcon>
+                        {!isCollapsed && <ListItemText primary="Output Parameters" />}
+                    </ListItemButton>
+                </Tooltip>
+                {!isCollapsed && (
+                    <ListItemButton>
+                        <div style={{ display: 'flex', gap: '10px', width: '100%' }}>
+                            <TextField
+                                id="output-name"
+                                label="Outputs"
+                                variant="outlined"
+                                size="small"
+                                style={{ flex: 1 }}
+                                margin="dense"
+                            />
+                            <TextField
+                                id="output-data-type"
+                                label="Data Type"
+                                variant="outlined"
+                                size="small"
+                                style={{ flex: 1 }}
+                                margin="dense"
+                            />
+                        </div>
+                    </ListItemButton>
+                )}
+                <Divider />
+                {/* Add Blocks */}
+                <Tooltip title="Add Blocks" placement="right">
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <AddCircleOutlineIcon />
+                        </ListItemIcon>
+                        {!isCollapsed && <ListItemText primary="Add Blocks" />}
+                    </ListItemButton>
+                </Tooltip>
+                {!isCollapsed && (
+                    <ListItemButton>
+                        <FormControl style={{ flex: 1, marginRight: '10px' }} size="small">
+                            <InputLabel id="block-type-label">Block Type</InputLabel>
+                            <Select
+                                labelId="block-type-label"
+                                id="block-type-select"
+                                value=""
+                                label="Block Type"
+                                fullWidth
+                            >
+                                <MenuItem value="While Loop">While Loop</MenuItem>
+                                <MenuItem value="If Statement">If Statement</MenuItem>
+                                <MenuItem value="If-Else Statement">If-Else Statement</MenuItem>
+                                <MenuItem value="For Loop">For Loop</MenuItem>
+                                <MenuItem value="Expression">Expression</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <Button variant="outlined" color="primary">
+                            Add
+                        </Button>
+                    </ListItemButton>
+                )}
             </List>
         </Drawer>
     );
