f25503eec8e274657f74aed72bcb0a92cb2b1675
khanabdu25 <khanabdu@umich.edu>
2023-12-03 12:44:47 -0500

    Better close animation

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,11 +1,11 @@
-import BackIcon from '@mui/icons-material/ArrowBackIos';
-import MenuIcon from '@mui/icons-material/Menu';
-import { Drawer, Toolbar, IconButton, Divider, List, ListItemButton, ListItemText } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import { useState } from 'react';
+import {  IconButton, Divider, List, ListItemButton, ListItemText } from '@mui/material';
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import MuiDrawer from '@mui/material/Drawer';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Select from '@mui/material/Select';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
@@ -16,84 +16,78 @@ import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import TransitEnterexitIcon from '@mui/icons-material/TransitEnterexit';
 import CallMadeIcon from '@mui/icons-material/CallMade';
 
+const drawerWidth = 240;
 
-const drawerWidth = 280;
-const thinDrawerWidth = 60;
-
-const openedMixin = (theme) => ({
+const openedMixin = (theme: Theme): CSSObject => ({
     width: drawerWidth,
     transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
     }),
     overflowX: 'hidden',
-});
-
-const closedMixin = (theme) => ({
+  });
+  
+  const closedMixin = (theme: Theme): CSSObject => ({
     transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
     }),
     overflowX: 'hidden',
-    width: thinDrawerWidth,
+    width: `calc(${theme.spacing(7)} + 1px)`,
     [theme.breakpoints.up('sm')]: {
-        width: thinDrawerWidth,
+      width: `calc(${theme.spacing(8)} + 1px)`,
     },
-});
+  });
 
-const Sidebar = () => {
-    const [isCollapsed, setIsCollapsed] = useState(false);
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  }));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+      boxSizing: 'border-box',
+      ...(open && {
+        ...openedMixin(theme),
+        '& .MuiDrawer-paper': openedMixin(theme),
+      }),
+      ...(!open && {
+        ...closedMixin(theme),
+        '& .MuiDrawer-paper': closedMixin(theme),
+      }),
+    }),
+  );
+
+  const Sidebar = ({ open, toggleDrawer }) => {
+
     const theme = useTheme();
 
-    const handleToggleDrawer = () => {
-        setIsCollapsed(!isCollapsed);
-    };
-
     return (
-        <Drawer
-            variant="persistent"
-            anchor='left'
-            open={true}
-            sx={{
-                ...(isCollapsed ? closedMixin(theme) : openedMixin(theme)),
-                '& .MuiDrawer-paper': {
-                    width: isCollapsed ? thinDrawerWidth : drawerWidth,
-                    boxSizing: 'border-box',
-                },
-            }}
-        >
-            <Toolbar
-                className='h-16'
-                style={{
-                    backgroundColor: theme.palette.primary.main,
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
-                <IconButton
-                    style={{
-                        color: theme.palette.primary.contrastText,
-                    }}
-                    onClick={handleToggleDrawer}
-                >
-                    {isCollapsed ? <MenuIcon /> : <BackIcon />}
-                </IconButton>
-            </Toolbar>
-
-
-            <Divider />
-            <List>
+        <Drawer variant="permanent" open={open}>
+        <DrawerHeader style={{ backgroundColor: theme.palette.primary.main }}>
+          <IconButton onClick={toggleDrawer}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
                 {/* Algorithm Name */}
                 <Tooltip title="Algorithm Name" placement="right">
                     <ListItemButton>
                         <ListItemIcon>
                             <LabelIcon />
                         </ListItemIcon>
-                        {!isCollapsed && <ListItemText primary="Algorithm Name" />}
+                        {open && <ListItemText primary="Algorithm Name" />}
                     </ListItemButton>
                 </Tooltip>
-                {!isCollapsed && (
+                {open && (
                     <ListItemButton>
                         <TextField
                             id="algorithm-name"
@@ -111,10 +105,10 @@ const Sidebar = () => {
                         <ListItemIcon>
                             <TransitEnterexitIcon />
                         </ListItemIcon>
-                        {!isCollapsed && <ListItemText primary="Input Parameters" />}
+                        {open && <ListItemText primary="Input Parameters" />}
                     </ListItemButton>
                 </Tooltip>
-                {!isCollapsed && (
+                {open && (
                     <ListItemButton>
                         <TextField
                             id="input-name"
@@ -141,10 +135,10 @@ const Sidebar = () => {
                         <ListItemIcon>
                             <CallMadeIcon />
                         </ListItemIcon>
-                        {!isCollapsed && <ListItemText primary="Output Parameters" />}
+                        {open && <ListItemText primary="Output Parameters" />}
                     </ListItemButton>
                 </Tooltip>
-                {!isCollapsed && (
+                {open && (
                     <ListItemButton>
                         <div style={{ display: 'flex', gap: '10px', width: '100%' }}>
                             <TextField
@@ -173,10 +167,10 @@ const Sidebar = () => {
                         <ListItemIcon>
                             <AddCircleOutlineIcon />
                         </ListItemIcon>
-                        {!isCollapsed && <ListItemText primary="Add Blocks" />}
+                        {open && <ListItemText primary="Add Blocks" />}
                     </ListItemButton>
                 </Tooltip>
-                {!isCollapsed && (
+                {open && (
                     <ListItemButton>
                         <FormControl style={{ flex: 1, marginRight: '10px' }} size="small">
                             <InputLabel id="block-type-label">Block Type</InputLabel>
@@ -200,8 +194,9 @@ const Sidebar = () => {
                     </ListItemButton>
                 )}
             </List>
-        </Drawer>
+      </Drawer>
     );
 };
 
 export default Sidebar;
+
