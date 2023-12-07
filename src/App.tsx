6d53ca5754eb5c9f0ff98f30d931ea84ae3ae345
Ali Bdeir <developer.ali.bdeir@gmail.com>
2023-12-06 20:57:24 -0500

    Fixed app code

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,17 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import MainPage from './pages/main/main-page';
+import Toolbar from './components/header/header';
 import Sidebar from './components/sidebar/sidebar';
-
-const drawerWidth = 240;
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
+import MainPage from './pages/main/main-page';
 
 export default function App() {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const toggleDrawer = () => {
-    setOpen(!open); // Toggle the open state
-  };
-
-  // const handleDrawerClose = () => {
-  //   setOpen(false);
-  // };
-
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar >
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={toggleDrawer}
-            edge="start"
-            sx={{
-              marginRight: 5,
-              ...(open && { display: 'none' }),
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Mini variant drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Sidebar open={open} toggleDrawer={toggleDrawer} />
-      <Box component="main" sx={{ flexGrow: 1, p: 2 }}>
-        <DrawerHeader />
-        <MainPage/>
-      </Box>
-    </Box>
-  );
+    return (
+        <div className='h-screen w-screen'>
+            <div className='flex flex-row w-full'>
+                <Sidebar open toggleDrawer={() => alert('')} />
+                <div className='flex flex-col flex-grow'>
+                    <Toolbar />
+                    <MainPage />
+                </div>
+            </div>
+        </div>
+    );
 }
