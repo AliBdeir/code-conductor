ad6269d3f23e99d83f2ad79a8d2fd4d2acf93c5d
Ali Bdeir <developer.ali.bdeir@gmail.com>
2023-12-12 14:57:34 -0500

    Persisted state

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,12 +3,43 @@ import { layoutReducers } from './slices/layout-slice'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { dataReducer } from './slices/data-slice'
 
+
+const loadState = (): RootState | undefined => {
+    try {
+      const serializedState = localStorage.getItem('state');
+      if (serializedState === null) {
+        return undefined;
+      }
+      return JSON.parse(serializedState);
+    } catch (err) {
+      return undefined;
+    }
+  };
+
+  export const saveState = (state: RootState) => {
+    try {
+      const serializedState = JSON.stringify(state);
+      localStorage.setItem('state', serializedState);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+const persistedState = loadState() as any;
+
 export const store = configureStore({
   reducer: combineReducers({
     layout: layoutReducers,
     data: dataReducer,
   }),
+  preloadedState: persistedState,
 })
+
+
+// Subscribe to store changes and save the state to localStorage
+store.subscribe(() => {
+    saveState(store.getState());
+  });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
